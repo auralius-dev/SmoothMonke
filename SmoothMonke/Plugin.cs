48ca93dc5de2cfd9083a863e6b34c3b78f65f6d5
Lillie <lennon.m.craig@gmail.com>
2023-04-05 23:49:56 -0400

    Remake most of the architecture
    Mod now uses Griac's template which means the project now has post-compile copying, easy scripts to make releases for MMM, among other things

---
+++ SmoothMonke/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace SmoothMonke
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start(){ Utilla.Events.GameInitialized += OnGameInitialized; }
+        
+        // Waits until after GT systems are intialized before attepting to do anything.
+        void OnGameInitialized(object sender, EventArgs e)  
+        {
+            foreach (GameObject obj in Resources.FindObjectsOfTypeAll<GameObject>()) // Use FindObjectsOfTypeAll to get all objects, including disabled objects, such as the cave map. Then just loop through them all.
+            {
+                try
+                {
+                    {
+                        foreach (Material mat in obj.GetComponent<Renderer>().materials) // Cycle through all materials on an object.
+                        {
+                            mat.mainTexture.filterMode = FilterMode.Bilinear; // Changing the FilterMode from point, to bilinear. Trilinear also works, but bilinear is all that's needed.
+                        }
+                    }
+                }
+                catch {}
+            }
+        }
+    }
+}
---
+++ SmoothMonke/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SmoothMonke
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.auralius.monkeytag.texturefiltering";
+        public const string Name = "Smooth Monke";
+        public const string Version = "2.0.0.0";
+    }
+}
